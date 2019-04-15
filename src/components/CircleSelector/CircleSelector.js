07c42f2014de737272f41899ac9ad36ccc618436
ursulacj <ursulacedillojohnson@gmail.com>
2019-04-15 16:11:05 -0500

    committing circle selector fix

--- src/components/CircleSelector/CircleSelector.js
+++ src/components/CircleSelector/CircleSelector.js
@@ -4,10 +4,10 @@ import './CircleSelector.css'
 const CircleSelector = (props) => {
     return (
         <div className="CircleSelector">
-            <button onClick={props.handleClick1} className={props.button1Selected ? 'selected' : 'unselected'}>Select Circle 1</button>
-            <button onClick={props.handleClick2} className={props.button2Selected ? 'selected' : 'unselected'}>Select Circle 2</button>
-            <button onClick={props.handleClick3} className={props.button3Selected ? 'selected' : 'unselected'}>Select Circle 3</button>
-            <button onClick={props.handleClick4} className={props.button4Selected ? 'selected' : 'unselected'}>Select Circle 4</button>
+            <button onClick={props.handleClick1} className={props.button1Selected ? 'selected' : 'unselected'}>{props.button1Selected ? 'Circle 1 Selected' : 'Select Circle 1'}</button>
+            <button onClick={props.handleClick2} className={props.button2Selected ? 'selected' : 'unselected'}>{props.button2Selected ? 'Circle 2 Selected' : 'Select Circle 2'}</button>
+            <button onClick={props.handleClick3} className={props.button3Selected ? 'selected' : 'unselected'}>{props.button3Selected ? 'Circle 3 Selected' : 'Select Circle 3'}</button>
+            <button onClick={props.handleClick4} className={props.button4Selected ? 'selected' : 'unselected'}>{props.button4Selected ? 'Circle 4 Selected' : 'Select Circle 4'}</button>
         </div>
     )
 }
